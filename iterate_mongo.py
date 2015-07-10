9ecfea47406b399425a80591a6d8941ef9b3b214
thadi <thadef@web.de>
2015-07-10 11:20:29 +0200

    interactive iteration

--- iterate_mongo.py
+++ iterate_mongo.py
@@ -1,4 +1,6 @@
 import pymongo
+from pprint import PrettyPrinter
+p = PrettyPrinter(4)
 
 #connect to localhost
 connection = pymongo.MongoClient('localhost', 27017)
@@ -12,23 +14,21 @@ relations = db.relations
 #entities collection
 entities = db.entities
 
-#comment in what collection you would like to iterate
-collection_to_iterate = relations
-#collection_to_iterate = entities
-
 #how many elements shall be iterated over? 0 for all
-limit = 1
-count = 0
-#find without filter arguments returns a cursor pointing at the first element of an list
-#containing all relation elements (since we didnt apply a filter)
-for current_object in collection_to_iterate.find():
-    #current_object contains the current object (bson)
-    #with current_object.get(<field_name>) you get the specefied field value
-    print(current_object)
-    if limit != 0:
-        count += 1
-        if count >= limit:
-            break
+limit = 5
+def iterate_collection(collection, limit):
+    count = 0
+    #find without filter arguments returns a cursor pointing at the first element of an list
+    #containing all relation elements (since we didnt apply a filter)
+    for current_object in collection.find():
+        #current_object contains the current object (bson)
+        #with current_object.get(<field_name>) you get the specefied field value
+        p.pprint(current_object)
+        raw_input("press enter for next entry")
+        if limit != 0:
+            count += 1
+            if count >= limit:
+                break
 '''
 gets a collection and returns a normalized list with unique values
 for a given key.
@@ -39,5 +39,6 @@ def get_items(data, key):
         items.add(row[key])
     return sorted([str(x.lower()) for x in list(items)])
 
-print get_items(relations, "type")
+#print get_items(entities, "type")
+iterate_collection(entities, 0)
 print('done')
